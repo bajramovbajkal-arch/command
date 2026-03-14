803cea3104b315278acdbc926bc06821bde26d8e
urmonaliyevv <urmonaliyevv@gmail.com>
2026-03-14 11:22:54 +0500

    urmonalievv finished

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import asyncio
 from aiogram import Bot, Dispatcher
-from aiogram.types import Message
+from aiogram.types import Message,InlineKeyboardMarkup,InlineKeyboardButton,CallbackQuery
 from aiogram.filters import CommandStart, Command
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
@@ -24,6 +24,50 @@ TOKEN = os.getenv("TOKEN")
 bot= Bot(token=TOKEN)
 dp= Dispatcher()
 
+inline_kb = InlineKeyboardMarkup(
+    inline_keyboard=[
+        [InlineKeyboardButton(text="📞 Command 1", callback_data="com_1"),
+        InlineKeyboardButton(text="📢 Command 2", callback_data="com_2")],
+
+        [InlineKeyboardButton(text="📞 Command 3", callback_data="com_3"),
+        InlineKeyboardButton(text="📢 Command 4", callback_data="com_4")],
+
+        [InlineKeyboardButton(text="📞 Command 5", callback_data="com_5")]
+    ]
+)
+
+@dp.message(CommandStart())
+async def start_handler(message: Message):
+    await message.answer(
+        "📋 Kerakli bo‘limni tanlang:",
+        reply_markup=inline_kb
+    )
+
+
+@dp.callback_query()
+async def callback_handler(callback: CallbackQuery):
+    data = callback.data
+
+    if data == "com_1":
+        await callback.message.answer("Salom")
+
+    elif data == "com_2":
+        await callback.message.answer("help")
+
+    elif data == "com_3":
+        await callback.message.answer("About")
+
+    elif data == "com_4":
+        await callback.message.answer(
+            "Reset"
+        )
+
+    elif data == "com_5":
+        await callback.message.answer(
+            "Future"
+        )
+
+    await callback.answer()
 
 async def main():
     print('bot ishladi')
